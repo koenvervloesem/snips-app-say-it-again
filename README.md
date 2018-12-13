d4d5cda5bffe9489aff7a2ee41f581e048750896
Koen Vervloesem <koen@vervloesem.eu>
2018-12-13 20:10:23 +0100

    Now also able to repeat the last captured text.

--- README.md
+++ README.md
@@ -1,16 +1,16 @@
 # Say it again skill for Snips 
-With this [Snips](https://snips.ai/) skill, you can ask your voice assistant to repeat its last message.
+With this [Snips](https://snips.ai/) skill, you can ask your voice assistant to repeat its last message or the last text it captured from your speech.
 
-The app is multi-room aware: it repeats the last message it has said on the site you are talking to.
+The app is multi-room aware: it repeats the last message it has said or heard on the site you are talking to.
 
 ## Installation
 The easiest way to install this skill is by adding the Snips app [Say it again](https://console.snips.ai/store/en/skill_YoV709qZP3n) to your assistant in the [Snips Console](https://console.snips.ai).
 
 ## Usage
-This app recognizes the following intent:
+This app recognizes the following intents:
 
-koan:SayItAgain
-:  The user asks to repeat the last message. The app responds by repeating the most recent message it uttered.
+  * koan:SayItAgain - The user asks to repeat the last message. The app responds by repeating the most recent message it uttered.
+  * koan:WhatDidISay - The user asks to repeat the last text Snips has captured. The app responds by repeating the most recent text it captured from the user's speech.
 
 ## Copyright
 This skill is provided by [Koen Vervloesem](mailto:koen@vervloesem.eu) as open source software. See LICENSE for more information.
--- action-say-it-again.py
+++ action-say-it-again.py
@@ -2,19 +2,26 @@
 # -*- coding: utf-8 -*-
 """
 This module contains a Snips skill that repeats the last message that
-your voice assistant has said on the site you are talking to.
+your voice assistant has said on the site you are talking to, as well as what
+Snips has understood from your last speech message.
 """
+
+from collections import deque
 
 import paho.mqtt.client as mqtt
 import json
 
 # Result sentence
-RESULT_SORRY = "Sorry, I don't remember what I said. I must have fallen asleep."
+RESULT_SAY_SORRY = "Sorry, I don't remember what I said. I must have fallen asleep."
+RESULT_TEXT_SORRY = "Sorry, I don't remember what you said. I must have fallen asleep."
+RESULT_TEXT = "I heard \"{}\" with likelihood {}."
 
 # MQTT topics
 TTS_SAY = "hermes/tts/say"
+ASR_TEXT_CAPTURED = "hermes/asr/textCaptured"
 DM_END_SESSION = "hermes/dialogueManager/endSession"
 INTENT_SAY_IT_AGAIN = "hermes/intent/koan:SayItAgain"
+INTENT_WHAT_DID_I_SAY = "hermes/intent/koan:WhatDidISay"
 
 # If this skill is supposed to run on the satellite,
 # please get this mqtt connection info from <config.ini>
@@ -26,7 +33,8 @@ MQTT_PORT = 1883
 class SayItAgain(object):
     """
     This skill repeats the last message that your voice assistant has said
-    on the site you are talking to.
+    on the site you are talking to, as well as what Snips has understood
+    from your last speech message.
     """
 
     def __init__(self):
@@ -36,30 +44,50 @@ class SayItAgain(object):
         # of each siteId.
         self.last_messages = {}
 
+        # Create an empty dictionary that will hold the two last captured texts
+        # and their likelihoods of each siteId.
+        self.last_texts = {}
+
         self.client = mqtt.Client()
         self.client.on_connect = self.subscribe_topics
-        self.client.on_message = self.handle_message
 
         self.client.connect(MQTT_HOST, MQTT_PORT, 60)
         self.client.loop_forever()
 
     def subscribe_topics(self, client, userdata, flags, rc):
         """Subscribe to the MQTT topics we're interested in."""
-        client.subscribe(TTS_SAY)
-        client.subscribe(INTENT_SAY_IT_AGAIN)
+        client.subscribe([(TTS_SAY, 0),
+                          (ASR_TEXT_CAPTURED, 0),
+                          (INTENT_SAY_IT_AGAIN, 0),
+                          (INTENT_WHAT_DID_I_SAY, 0)])
 
-    def handle_message(self, client, userdata, msg):
-        """Handle the messages we have subscribed to."""
-
-        if msg.topic == TTS_SAY:
-            self.handle_say(client, userdata, msg)
-        elif msg.topic == INTENT_SAY_IT_AGAIN:
-            self.handle_say_again(client, userdata, msg)
+        client.message_callback_add(TTS_SAY,
+                                    self.handle_say)
+        client.message_callback_add(ASR_TEXT_CAPTURED,
+                                    self.handle_text)
+        client.message_callback_add(INTENT_SAY_IT_AGAIN,
+                                    self.handle_say_again)
+        client.message_callback_add(INTENT_WHAT_DID_I_SAY,
+                                    self.handle_what_did_i_say)
 
     def handle_say(self, client, userdata, msg):
         """When Snips says something, save the text."""
         payload = json.loads(msg.payload)
         self.last_messages[payload["siteId"]] = payload["text"]
+
+    def handle_text(self, client, userdata, msg):
+        """When Snips captures a text, save it together with is likelihood."""
+        # We save the last two texts in a deque because the WhatDidISay intent
+        # also generates a captured text and we're not interested in that...
+        payload = json.loads(msg.payload)
+        if payload["siteId"] not in self.last_texts:
+            # Create a new deque of length 2.
+            self.last_texts[payload["siteId"]] = deque(maxlen=2)
+
+        # Add the text and likelihood at the end of the deque.
+        text = payload["text"]
+        likelihood = round(payload["likelihood"], 2)
+        self.last_texts[payload["siteId"]].append((text, likelihood))
 
     def handle_say_again(self, client, userdata, msg):
         """When the user asks to repeat the last message, do it."""
@@ -75,7 +103,28 @@ class SayItAgain(object):
             # If there is no previous message for this siteId,
             # tell the user we're sorry.
             client.publish(DM_END_SESSION,
-                           json.dumps({'text': RESULT_SORRY,
+                           json.dumps({'text': RESULT_SAY_SORRY,
+                                       'sessionId': payload["sessionId"]})
+                           )
+
+    def handle_what_did_i_say(self, client, userdata, msg):
+        """When the user asks to repeat the last captured text, do it."""
+        payload = json.loads(msg.payload)
+        if len(self.last_texts[payload["siteId"]]) == 2:
+            # If we have saved two previous texts for this siteId,
+            # repeat the first one.
+            # The second one is the one that generated the current intent.
+            last_text, last_likelihood = self.last_texts[payload["siteId"]][0]
+            client.publish(DM_END_SESSION,
+                           json.dumps({'text': RESULT_TEXT.format(last_text,
+                                                                  last_likelihood),
+                                       'sessionId': payload["sessionId"]})
+                           )
+        else:
+            # If there is no previous text for this siteId,
+            # tell the user we're sorry.
+            client.publish(DM_END_SESSION,
+                           json.dumps({'text': RESULT_TEXT_SORRY,
                                        'sessionId': payload["sessionId"]})
                            )
 
