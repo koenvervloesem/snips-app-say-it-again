4d61a18b6591c79966aa496d04bc744cffb72e57
philipp2310 <41761223+philipp2310@users.noreply.github.com>
2018-12-21 21:53:34 +0100

    Create de.py

---
+++ locale/de.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+"""
+This module contains the result sentences and intents for the English version
+of the Say it again skill.
+"""
+
+# Result sentences
+RESULT_SAY_SORRY = "Sorry, I don't remember what I said. I must have fallen asleep."
+RESULT_TEXT_SORRY = "Sorry, I don't remember what you said. I must have fallen asleep."
+RESULT_TEXT = "I heard \"{0}\" with likelihood {1}."
+RESULT_TEXT_NOTHING = "Sorry, I didn't hear anything."
+
+# Intents
+INTENT_SAY_IT_AGAIN = "hermes/intent/koan:SayItAgain"
+INTENT_WHAT_DID_I_SAY = "hermes/intent/koan:WhatDidISay"
