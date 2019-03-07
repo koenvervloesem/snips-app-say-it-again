736889df85dc999bc66fbebbc5795c38a48695f7
Koen Vervloesem <koen@vervloesem.eu>
2019-03-07 21:04:56 +0100

    Migrate to the Snipskit library.

--- action-say-it-again.py
+++ action-say-it-again.py
@@ -11,24 +11,25 @@ from collections import deque
 import importlib
 import json
 
-import paho.mqtt.client as mqtt
-import toml
+from snipskit.apps import MQTTSnipsApp
+from snipskit.app_decorators import topic
+
 
 # MQTT topics
 TTS_SAY = "hermes/tts/say"
 ASR_TEXT_CAPTURED = "hermes/asr/textCaptured"
 DM_END_SESSION = "hermes/dialogueManager/endSession"
-INTENT_MQTT = "hermes/intent/#"
+INTENT_ALL = "hermes/intent/#"
 
 
-class SayItAgain(object):
+class SayItAgain(MQTTSnipsApp):
     """
     This skill repeats the last message that your voice assistant has said
     on the site you are talking to, as well as what Snips has understood
     from your last speech message.
     """
 
-    def __init__(self):
+    def initialize(self):
         """Initialize the skill."""
 
         # Create an empty dictionary that will hold the last message
@@ -44,52 +45,17 @@ class SayItAgain(object):
         self.last_intent = {}
 
         # Use the assistant's language.
-        with open("/usr/share/snips/assistant/assistant.json") as json_file:
-            language = json.load(json_file)["language"]
+        self.i18n = importlib.import_module('translations.' + self.assistant['language'])
 
-        self.i18n = importlib.import_module("translations." + language)
 
-        # Get the MQTT host and port from /etc/snips.toml.
-        try:
-            mqtt_host_port = toml.load('/etc/snips.toml')['snips-common']['mqtt']
-            mqtt_host, mqtt_port = mqtt_host_port.split(':')
-            mqtt_port = int(mqtt_port)
-        except (KeyError, ValueError):
-            # If the mqtt key doesn't exist or doesn't have the correct format,
-            # use the default values.
-            mqtt_host = 'localhost'
-            mqtt_port = 1883
-
-        self.client = mqtt.Client()
-        self.client.on_connect = self.subscribe_topics
-
-        self.client.connect(mqtt_host, mqtt_port, 60)
-        self.client.loop_forever()
-
-    def subscribe_topics(self, client, userdata, flags, rc):
-        """Subscribe to the MQTT topics we're interested in."""
-        client.subscribe([(TTS_SAY, 0),
-                          (ASR_TEXT_CAPTURED, 0),
-                          (INTENT_MQTT, 0)])  # This captures all intents.
-
-        client.message_callback_add(TTS_SAY,
-                                    self.handle_say)
-        client.message_callback_add(ASR_TEXT_CAPTURED,
-                                    self.handle_text)
-        client.message_callback_add(INTENT_MQTT,
-                                    self.handle_intent)
-        client.message_callback_add(self.i18n.INTENT_SAY_IT_AGAIN,
-                                    self.handle_say_again)
-        client.message_callback_add(self.i18n.INTENT_WHAT_DID_I_SAY,
-                                    self.handle_what_did_i_say)
-        client.message_callback_add(self.i18n.INTENT_REPEAT_ACTION,
-                                    self.handle_repeat_action)
-
+    @topic(TTS_SAY)
     def handle_say(self, client, userdata, msg):
         """When Snips says something, save the text."""
         payload = json.loads(msg.payload.decode('utf-8'))
         self.last_messages[payload["siteId"]] = payload["text"]
 
+    
+    @topic(ASR_TEXT_CAPTURED)
     def handle_text(self, client, userdata, msg):
         """When Snips captures a text, save it together with its likelihood."""
         # We save the last two texts in a deque because the WhatDidISay intent
@@ -104,6 +70,8 @@ class SayItAgain(object):
         likelihood = round(payload["likelihood"], 2)
         self.last_texts[payload["siteId"]].append((text, likelihood))
 
+
+    @topic(INTENT_ALL)
     def handle_intent(self, client, userdata, msg):
         """
         When an intent is triggered (and it is not the RepeatAction intent!),
@@ -115,6 +83,8 @@ class SayItAgain(object):
             payload = json.loads(msg.payload.decode('utf-8'))
             self.last_intent[payload["siteId"]] = msg
 
+
+    @topic(self.i18n.INTENT_REPEAT_ACTION)
     def handle_repeat_action(self, client, userdata, msg):
         """Get the last captured intent and repeat."""
         payload = json.loads(msg.payload.decode('utf-8'))
@@ -131,6 +101,8 @@ class SayItAgain(object):
                                        'sessionId': payload["sessionId"]})
                            )
 
+
+    @topic(self.i18n.INTENT_SAY_IT_AGAIN)
     def handle_say_again(self, client, userdata, msg):
         """When the user asks to repeat the last message, do it."""
         payload = json.loads(msg.payload.decode('utf-8'))
@@ -149,6 +121,8 @@ class SayItAgain(object):
                                        'sessionId': payload["sessionId"]})
                            )
 
+
+    @topic(self.i18n.INTENT_WHAT_DID_I_SAY)
     def handle_what_did_i_say(self, client, userdata, msg):
         """When the user asks to repeat the last captured text, do it."""
         payload = json.loads(msg.payload.decode('utf-8'))
