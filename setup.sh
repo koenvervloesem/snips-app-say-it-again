f3cafbb13935c1a3afba4f91edf0db4237e62953
Koen Vervloesem <koen@vervloesem.eu>
2019-03-26 21:18:04 +0100

    setup.sh: Cleanup

--- setup.sh
+++ setup.sh
@@ -1,18 +1,16 @@
-#/usr/bin/env bash -e
+#!/usr/bin/env bash
+set -e
 
-PYTHON=`which python3`
+PYTHON=$(command -v python3)
 VENV=venv
 
-if [ -f "$PYTHON" ]
-then
+if [ -f "$PYTHON" ]; then
 
-    if [ ! -d $VENV ]
-    then
+    if [ ! -d $VENV ]; then
         # Create a virtual environment if it doesn't exist.
         $PYTHON -m venv $VENV
     else
-        if [ -e $VENV/bin/python2 ]
-        then
+        if [ -e $VENV/bin/python2 ]; then
             # If a Python2 environment exists, delete it first
             # before creating a new Python 3 virtual environment.
             rm -r $VENV
@@ -21,6 +19,7 @@ then
     fi
 
     # Activate the virtual environment and install requirements.
+    # shellcheck disable=SC1090
     . $VENV/bin/activate
     pip3 install -r requirements.txt
 
